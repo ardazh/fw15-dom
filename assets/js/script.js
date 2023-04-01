b4165867264b4bc2ee1eb9ebaa723999ddd0be1e
ardazh <127732352+ardazh@users.noreply.github.com>
2023-04-01 19:18:22 +0700

    menambah DOM pada input password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const inputEmail = document.getElementById('email')
+const inputPass = document.getElementById('password')
 
 inputEmail.addEventListener('keyup', function(event){
     inputEmail.style.border = '1px solid #C1C5D0'
@@ -9,6 +10,19 @@ inputEmail.addEventListener('keyup', function(event){
             inputEmail.style.border = '1px solid red'
         }else{
             inputEmail.style.border = '1px solid green'
+        }
+    }
+})
+
+inputPass.addEventListener('keyup', function(event){
+    inputPass.style.border = '1px solid #C1C5D0'
+    if(event.target.value === ""){
+        inputPass.style.border = '1px solid red'
+    } else{
+        if(event.target.value.length < 6){
+            inputPass.style.border = '1px solid red'
+        }else{
+            inputPass.style.border = '1px solid green'
         }
     }
 })
